12e685e830b1580acfc486720a767380afb4568d
Park JunYong <dyden119@gmail.com>
2024-04-07 21:19:23 +0900

    feat: 感情点数によってリストフィルタリング

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -28,6 +28,13 @@ const DiaryList = ({ diaryList }) => {
   const [filter, setFilter] = useState("all");
 
   const getProcessedDiaryList = () => {
+    const filterCallBack = (item) => {
+      if (filter === "good") {
+        return parseInt(item.emotion) <= 3;
+      } else {
+        return parseInt(item.emotion) > 3;
+      }
+    };
     const compare = (a, b) => {
       if (sortType === "lastest") {
         return parseInt(b.date) - parseInt(a.date);
@@ -36,7 +43,9 @@ const DiaryList = ({ diaryList }) => {
       }
     };
     const copyList = JSON.parse(JSON.stringify(diaryList));
-    const sortedList = copyList.sort(compare);
+    const filteredList =
+      filter === "all" ? copyList : copyList.filter((it) => filterCallBack(it));
+    const sortedList = filteredList.sort(compare);
     return sortedList;
   };
 
@@ -47,8 +56,15 @@ const DiaryList = ({ diaryList }) => {
         onChange={setSortType}
         optionList={sortOptionList}
       />
+      <ControlMenu
+        value={filter}
+        onChange={setFilter}
+        optionList={filterOptionList}
+      />
       {getProcessedDiaryList().map((it) => (
-        <div key={it.id}>{it.content}</div>
+        <div key={it.id}>
+          {it.content} {it.emotion}
+        </div>
       ))}
     </div>
   );
